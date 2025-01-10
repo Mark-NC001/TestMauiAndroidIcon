8707ecd3df3349d3d1fc9c1b755ee0b07003d1fb
Mark <75133432+Mark-NC001@users.noreply.github.com>
2025-01-10 13:44:46 +0000

    Add files via upload

---
+++ TestMauiAndroidIcon/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TestMauiAndroidIcon
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
